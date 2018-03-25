ceb11ff8971ef3e9d4cd4631bc602df9738f4e93
Jon Cook <jonpatrickcook@gmail.com>
2018-03-25 09:35:47 -0700

    done testing putstr

--- Libft/ft_putstr.c
+++ Libft/ft_putstr.c
@@ -1,10 +1,5 @@
 #include "libft.h"
 
-void ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
 void ft_putstr(char const *s)
 {
 	int i;
@@ -14,10 +9,4 @@ void ft_putstr(char const *s)
 		ft_putchar(s[i]);
 		i++;
 	}
-}
-
-int main(void)
-{
-	ft_putstr("hello\n");
-	return (0);
 }
